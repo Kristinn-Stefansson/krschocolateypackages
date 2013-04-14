35ff1c0d96f6f16b05d9ff1b406b15b775d555cf
Kristinn Stefansson <Kristinn.Stefansson@gmail.com>
2013-04-14 18:03:32 +0000

    Add automatic package for Scan Tailor
    Updated Ketarin settings

---
+++ _output/ScanTailor/0.9.11.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'ScanTailor'
+$installerType = 'EXE' 
+$url = 'http://downloads.sourceforge.net/project/scantailor/scantailor/0.9.11.1/scantailor-0.9.11.1-32bit-install.exe?r=&ts=1365961462&use_mirror=switch' # download url
+$url64 = 'http://downloads.sourceforge.net/project/scantailor/scantailor/0.9.11.1/scantailor-0.9.11.1-64bit-install.exe?r=&ts=1365961471&use_mirror=switch' # 64bit URL uses the same as $url
+$silentArgs = '/S' # NSIS installar uses /S 
+$validExitCodes = @(0) 
+
+# installer, will assert administrative rights
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
