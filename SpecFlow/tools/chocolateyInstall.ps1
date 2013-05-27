1da49752985af1a17a7917f9e6f6852a572e8b19
Kristinn Stefansson <Kristinn.Stefansson@gmail.com>
2013-05-27 01:52:06 +0000

    Fixed Specflow package

--- SpecFlow/tools/chocolateyInstall.ps1
+++ SpecFlow/tools/chocolateyInstall.ps1
@@ -1,11 +1,9 @@
-﻿$packageName = 'PSWindowsUpdate' # arbitrary name for the package, used in messages
+﻿$packageName = 'SpecFlow' # arbitrary name for the package, used in messages
 $url = '{{DownloadUrl}}' # download url
 $url64 = '{{DownloadUrl}}' # 64bit URL 
 
 try { 
-  $installDir = Join-Path $PSHome "Modules"
-
-  Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" "$url64"
+  Install-ChocolateyVsixPackage "$packageName" "$url"
      
   Write-ChocolateySuccess "$packageName"
 } catch {
--- _output/SpecFlow/1.9.2/tools/chocolateyInstall.ps1
+++ _output/SpecFlow/1.9.2/tools/chocolateyInstall.ps1
@@ -1,11 +1,9 @@
-﻿$packageName = 'PSWindowsUpdate' # arbitrary name for the package, used in messages
+﻿$packageName = 'SpecFlow' # arbitrary name for the package, used in messages
 $url = 'http://visualstudiogallery.msdn.microsoft.com/9915524d-7fb0-43c3-bb3c-a8a14fbd40ee/file/79327/7/TechTalk.SpecFlow.Vs2010Integration.vsix' # download url
 $url64 = 'http://visualstudiogallery.msdn.microsoft.com/9915524d-7fb0-43c3-bb3c-a8a14fbd40ee/file/79327/7/TechTalk.SpecFlow.Vs2010Integration.vsix' # 64bit URL 
 
 try { 
-  $installDir = Join-Path $PSHome "Modules"
-
-  Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" "$url64"
+  Install-ChocolateyVsixPackage "$packageName" "$url"
      
   Write-ChocolateySuccess "$packageName"
 } catch {
