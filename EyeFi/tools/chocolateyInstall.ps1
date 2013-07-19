486ff2ff72dfb4222da31ce40586af3e80866d02
Kristinn Stefánsson <kristinn.stefansson@gmail.com>
2013-07-19 15:25:56 +0000

    Update PSWindowsUpdate package, add EyeFi and licence

---
+++ EyeFi/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'EyeFi'
+$installerType = 'EXE' 
+$url = '{{DownloadUrl}}' # download url
+$url64 = '{{DownloadUrl}}' # 64bit URL uses the same as $url
+$silentArgs = '/Q REBOOT=ReallySuppress' # NSIS installar uses /S, MSI /Q
+$validExitCodes = @(0) 
+
+# installer, will assert administrative rights
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
---
+++ _output/EyeFi/3.4.26/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'EyeFi'
+$installerType = 'EXE' 
+$url = 'http://support.eye.fi/agent/download/3.4.26/Setup.exe' # download url
+$url64 = 'http://support.eye.fi/agent/download/3.4.26/Setup.exe' # 64bit URL uses the same as $url
+$silentArgs = '/Q REBOOT=ReallySuppress' # NSIS installar uses /S, MSI /Q
+$validExitCodes = @(0) 
+
+# installer, will assert administrative rights
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
