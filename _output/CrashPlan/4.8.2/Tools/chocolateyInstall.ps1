f5e2c8b3ec9b0dc52c929b81b6ade58f32efc1ce
Kristinn-Stefansson <kristinn.stefansson@gmail.com>
2017-04-30 20:31:16 +0000

    Crashplan 4.8.2

---
+++ _output/CrashPlan/4.8.2/Tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'CrashPlan'
+$installerType = 'MSI' 
+$url = 'http://download.crashplan.com/installs/win/install/CrashPlan/jre/CrashPlan_4.8.2_Win.msi' # download url
+$url64 = 'http://download.crashplan.com/installs/win/install/CrashPlan/jre/CrashPlan_4.8.2_Win64.msi' # 64bit URL 
+$silentArgs = '/quiet /norestart' # 
+$validExitCodes = @(0,3010) 
+
+# installer, will assert administrative rights
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
